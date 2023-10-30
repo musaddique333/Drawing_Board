6340f2a77ec8ed26df3626fd212ee2669348f37b
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2023-10-30 10:38:07 +0000

    adding the code to github

---
+++ app.js
@@ -0,0 +1,112 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const colorPicker = document.getElementById("colorPicker");
+    const rainbowBtn = document.getElementById("rainbowBtn");
+    const eraserBtn = document.getElementById("eraserBtn");
+    const clearBtn = document.getElementById("clearBtn");
+    const showPixels = document.getElementById("showPixels");
+    const totalPixels = document.getElementById("totalPixels");
+    const container = document.getElementById("container");
+    const pixel = document.getElementsByClassName("pixel");
+
+    let defaultColor = "#000000";
+    let defaultPixels = 32;
+
+    let color = defaultColor;
+    let pixels = defaultPixels;
+    let rainbow = false;
+    let eraser = false;
+
+    totalPixels.addEventListener("input", updatePixelScreen, false);
+    totalPixels.addEventListener("change", updatePixels, false);
+
+
+    function updatePixelScreen(event) {
+        pixels = event.target.value;
+        showPixels.innerHTML = `${pixels} x ${pixels}`;
+    }
+
+    function updatePixels() {
+        pixels = totalPixels.value;
+        container.innerHTML = "";
+        clearGrid();
+    }
+
+    function clearGrid() {
+        container.innerHTML = "";
+        createGrid();
+    };
+
+    colorPicker.addEventListener("input", updateColor, false);
+    
+    function updateColor(event) {
+        color = event.target.value;
+        rainbow = false;
+        updateCanvas();
+    }
+
+    rainbowBtn.addEventListener("click", function(event) {
+        updateBtnColor(event);
+        rainbow = true;
+        eraser = false;
+        updateCanvas();
+    });
+
+    eraserBtn.addEventListener("click", function(event) {
+        updateBtnColor(event);
+        eraser = true;
+        rainbow = false;
+        updateCanvas();
+    });
+
+    clearBtn.addEventListener("click", function(event) {
+        updateBtnColor(event);
+        clearGrid();
+    });
+
+    function updateBtnColor(event) {
+
+        rainbowBtn.style.cssText = "background-color: #b3cde0; border: solid 0.15vmax #005b96;; color: #005b96;";
+        eraserBtn.style.cssText = "background-color: #b3cde0; border: solid 0.15vmax #005b96;; color: #005b96;";
+        clearBtn.style.cssText = "background-color: #b3cde0; border: solid 0.15vmax #005b96;; color: #005b96;";
+        event.target.style.cssText = "background-color: #005b96; border: solid 0.15vmax #011f4b; color: #b3cde0;";
+    }
+
+    function updateCanvas() {
+        function draw(event) {
+            if(rainbow){
+                let colorR = `${Math.floor(Math.random() * 255)}`;
+                let colorG = `${Math.floor(Math.random() * 255)}`;
+                let colorB = `${Math.floor(Math.random() * 255)}`;
+                let colorA = `1`;
+                event.target.style.backgroundColor = `rgba(${colorR}, ${colorG}, ${colorB}, ${colorA})`;
+                console.log("i am in rainbow");
+            }
+            else if(eraser){
+                event.target.style.backgroundColor = "#ffffff";
+            }
+            else{
+                event.target.style.backgroundColor = color;
+                rainbow = false;
+            }
+        }
+        Array.from(pixel).forEach(element => {
+            element.addEventListener("mouseover", draw, false);
+            element.addEventListener("touchmove", draw, false);
+        });
+    }
+
+
+    function createGrid(){
+        container.innerHTML = "";
+        for(let i= 0; i < pixels * pixels; i++){
+            let element = document.createElement("div");
+            element.classList.add("pixel");
+            element.style.width = `${(500 / pixels)}px`;
+            element.style.height = `${(500 / pixels)}px`;
+            container.appendChild(element);
+        }
+        updateCanvas();
+    }
+
+    createGrid();
+});
